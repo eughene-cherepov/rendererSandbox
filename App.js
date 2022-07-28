cb7884ef38e5eaaedf29ce30b5d35af4753ef171
eughene-cherepov <e.ch.vas@hotmail.com>
2022-07-28 11:31:27 +0300

    refactor/move map to the separate component and add eslint rules

--- App.js
+++ App.js
@@ -3,53 +3,7 @@ import {SafeAreaView, StyleSheet, useColorScheme, View} from 'react-native';
 
 import {Colors} from 'react-native/Libraries/NewAppScreen';
 
-import MapboxGL, {Logger} from '@rnmapbox/maps';
-
-Logger.setLogCallback(log => {
-  const {message} = log;
-
-  // expected warnings - see https://github.com/mapbox/mapbox-gl-native/issues/15341#issuecomment-522889062
-  if (
-    message.match('Request failed due to a permanent error: Canceled') ||
-    message.match('Request failed due to a permanent error: Socket Closed')
-  ) {
-    return true;
-  }
-  return false;
-});
-
-MapboxGL.setAccessToken('');
-
-const defaultStyle = {
-  version: 8,
-  name: 'Land',
-  sources: {
-    map: {
-      type: 'raster',
-      tiles: ['https://a.tile.openstreetmap.org/{z}/{x}/{y}.png'],
-      tileSize: 256,
-      minzoom: 1,
-      maxzoom: 19,
-    },
-  },
-  layers: [
-    {
-      id: 'background',
-      type: 'background',
-      paint: {
-        'background-color': '#f2efea',
-      },
-    },
-    {
-      id: 'map',
-      type: 'raster',
-      source: 'map',
-      paint: {
-        'raster-fade-duration': 100,
-      },
-    },
-  ],
-};
+import {Map} from './src/components/Map/Map';
 
 const App = () => {
   const isDarkMode = useColorScheme() === 'dark';
@@ -60,21 +14,18 @@ const App = () => {
 
   return (
     <SafeAreaView style={backgroundStyle}>
-      <View
-        style={{
-          backgroundColor: 'red',
-          height: '100%',
-          width: '100%',
-        }}>
-        <MapboxGL.MapView
-          style={{height: '100%', width: '100%'}}
-          styleJSON={JSON.stringify(defaultStyle)}
-        />
+      <View style={styles.mapWrapper}>
+        <Map />
       </View>
     </SafeAreaView>
   );
 };
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  mapWrapper: {
+    height: '100%',
+    width: '100%',
+  },
+});
 
 export default App;
---
+++ src/components/Map/Map.js
@@ -0,0 +1,63 @@
+import React from 'react';
+import {StyleSheet} from 'react-native';
+import MapboxGL, {Logger} from '@rnmapbox/maps';
+
+MapboxGL.setAccessToken('');
+
+Logger.setLogCallback(log => {
+  const {message} = log;
+
+  // expected warnings - see https://github.com/mapbox/mapbox-gl-native/issues/15341#issuecomment-522889062
+  if (
+    message.match('Request failed due to a permanent error: Canceled') ||
+    message.match('Request failed due to a permanent error: Socket Closed')
+  ) {
+    return true;
+  }
+  return false;
+});
+
+const defaultStyle = {
+  version: 8,
+  name: 'Land',
+  sources: {
+    map: {
+      type: 'raster',
+      tiles: ['https://a.tile.openstreetmap.org/{z}/{x}/{y}.png'],
+      tileSize: 256,
+      minzoom: 1,
+      maxzoom: 19,
+    },
+  },
+  layers: [
+    {
+      id: 'background',
+      type: 'background',
+      paint: {
+        'background-color': '#f2efea',
+      },
+    },
+    {
+      id: 'map',
+      type: 'raster',
+      source: 'map',
+      paint: {
+        'raster-fade-duration': 100,
+      },
+    },
+  ],
+};
+
+export const Map = () => (
+  <MapboxGL.MapView
+    style={styles.map}
+    styleJSON={JSON.stringify(defaultStyle)}
+  />
+);
+
+const styles = StyleSheet.create({
+  map: {
+    height: '100%',
+    width: '100%',
+  },
+});
